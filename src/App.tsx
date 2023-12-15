20b982599247e74c1c0716ae544a552aa4537dc5
Katya <ekaterina.gnedenko@gmail.com>
2023-12-15 23:44:42 +0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Counter } from './components/Counter'
+import { Routes, Route, Link } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
 import './index.scss'
 
 function App() {
   return (
     <div className='app'>
-      Text
-      <Counter/>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Routes>
+        <Route path={'/about'} element={<AboutPage />}/>
+        <Route path={'/'} element={<MainPage />}/>
+      </Routes>
     </div>
   )
 }

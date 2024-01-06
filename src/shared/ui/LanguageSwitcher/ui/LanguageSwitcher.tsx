49cb0503063bc623f7c3bde18edeaff0f9082f72
Katya <ekaterina.gnedenko@gmail.com>
2024-01-06 04:13:05 +0300

    add stylelint, eslint for i18next

--- src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.tsx
+++ src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.tsx
@@ -1,7 +1,6 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './LanguageSwitcher.module.scss';
 import { useTranslation } from 'react-i18next';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 interface LanguageSwitcherProps {
   className?: string;
@@ -16,7 +15,7 @@ export const LanguageSwitcher = ({ className }: LanguageSwitcherProps) => {
 
   return (
       <Button
-        className={classNames(cls.LanguageSwitcher, {}, [className])}
+        className={classNames('', {}, [className])}
         theme={ThemeButton.CLEAR}
         onClick={toggle}
       >
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,8 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
-import cls from './ThemeSwitcher.module.scss';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/sun.svg';
 import DarkIcon from 'shared/assets/icons/moon.svg';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -15,7 +14,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
   return (
     <Button
       theme={ThemeButton.CLEAR}
-      className={classNames(cls.ThemeSwitcher, {}, [className])}
+      className={classNames('', {}, [className])}
       onClick={toggleTheme}
     >
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}

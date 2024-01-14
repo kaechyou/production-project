edffe53c5e6821e44f30ffb91f146fa5da113930
Katya <ekaterina.gnedenko@gmail.com>
2024-01-15 02:12:10 +0300

    feat: add loader and not found page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import './styles/index.scss'
+import './styles/index.scss';
 
 const App = () => {
   const { theme } = useTheme();
@@ -13,13 +13,13 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
-        <div className='content-page'>
+        <div className="content-page">
           <Sidebar />
           <AppRouter />
         </div>
       </Suspense>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,9 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 function MainPage() {
   const { t } = useTranslation();
 
-  return (
-    <div>
-      {t('new-translate')}
-      {t('main-page')}
-    </div>
-  )
+  return <div>{t('main-page')}</div>;
 }
 
 export default MainPage;

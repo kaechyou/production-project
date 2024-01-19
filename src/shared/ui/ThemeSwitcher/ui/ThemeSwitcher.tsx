e6a7adc1a1a9120416fa364d5bad2ef480599def
Katya <ekaterina.gnedenko@gmail.com>
2024-01-19 15:38:54 +0300

    add default export for Button

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ThemeButton } from 'shared/ui/Button';
 import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/sun.svg';
 import DarkIcon from 'shared/assets/icons/moon.svg';
